aef7c14fa8597cd6cb00a956729e06580095bd01
IHOR <85029363+ihorutkin@users.noreply.github.com>
2022-12-10 16:51:00 +0100

    todo application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './todo/todoList';
+import React, { useEffect, useState } from 'react';
+import Context from './context';
+import Loader from './loader';
+import Modal from './modal/Modal';
+
+const AddTodo = React.lazy(() => new Promise(resolve => {
+  setTimeout(() => {
+    resolve(import('./todo/addTodo'))
+  }, 3000)
+}))
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id){
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title){
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{removeTodo}}>
+
+      <div className='wrapper'>
+      <h1>Welcome to React</h1>
+      <Modal />
+
+      <React.Suspense fallback={<Loader />} >
+        <AddTodo onCreate={addTodo}/>
+      </React.Suspense>
+
+      {loading && <Loader />}
+
+      {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : (loading ? null : <p>No todos</p>)}
+      </div>
+
+    </Context.Provider>
   );
 }
 
